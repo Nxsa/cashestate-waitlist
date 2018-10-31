0b55e6ff4cc2a59de0c8cad66e64fe7a0f8987c7
Nosa <officiallyprime@gmail.com>
2018-10-31 18:13:46 +0100

    Final Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,41 @@
-$(document).ready(function () {
-    $('.overview-table').DataTable({
-        lengthChange: false,
-        info: false,
-        paging: false,
-        pageLength: 8,
-        ordering: false,
-        autoWidth: false,
-    });
-    $('.table-full').DataTable({
-        lengthChange: false,
-        pageLength: 16,
-        pagingType: 'numbers',
-        ordering: false,
-        autoWidth: false,
-    });
-});
+function checkScroll() {
+  var startY = $(".header-wrap").height() * 1; //The point where the navbar changes in px
 
+  if ($(window).scrollTop() > startY) {
+    $(".header-wrap").addClass("scrolled");
+  } else {
+    $(".header-wrap").removeClass("scrolled");
+  }
+}
 
-var menuCollapse = function () {
-    sidebar.classList.toggle("sidebar--toggle");
+if ($(".header-wrap").length > 0) {
+  $(window).on("scroll load resize", function() {
+    checkScroll();
+  });
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("modal-ctrl")[0];
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
 };
-var sidenav = document.getElementsByClassName("svg--nav")[0];
-var sidebar = document.getElementsByClassName("sidebar")[0];
-var toggleClose = document.getElementsByClassName("dashboard")[0];
 
-sidenav.addEventListener("click", menuCollapse);
-toggleClose.addEventListener("click", function () {
-    sidebar.classList.remove("sidebar--toggle");
-});
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
